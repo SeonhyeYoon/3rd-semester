98a5914144e65b5706630fc5f71d2eb3148940e2
SeonhyeYoon <ysunh118@gmail.com>
2021-09-17 01:46:45 -0700

    DMIT2008 files re-setting completed

--- DMIT 2008/front-end-framework/src/js/index.js
+++ DMIT 2008/front-end-framework/src/js/index.js
@@ -1,1 +1,11 @@
- 
+/* 
+      window DOMContentLoaded
+      html download but not external resources....
+
+      window load event triggers when the DOM is fully downloaded
+      img scripts etc....
+*/
+window.addEventListener('load', function(e){
+    // kick off my app.....
+    // document.body.style.backgroundColor='tomato'
+})
