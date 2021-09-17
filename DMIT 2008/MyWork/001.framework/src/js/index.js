e312fb3aaf34378bdd3211234e3c2a04f6780b56
SeonhyeYoon <ysunh118@gmail.com>
2021-09-16 23:40:54 -0700

    First Upload

---
+++ DMIT 2008/MyWork/001.framework/src/js/index.js
@@ -0,0 +1,12 @@
+/*
+    window DOMContectLoaded
+    html download but not external resources...
+
+    window load event triggers when the DOM is fully downloaded
+    img scripts etc...
+*/
+
+window.addEventListener('load', function(e){
+  // kick off my app...
+  //document.body.style.backgroundColor = 'tomato';
+})
