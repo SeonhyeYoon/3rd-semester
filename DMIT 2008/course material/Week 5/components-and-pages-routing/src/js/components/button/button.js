a3a238bfac19f3f9e15026186580e6f376cb1380
SeonhyeYoon <ysunh118@gmail.com>
2021-10-17 21:59:54 -0700

    DMIT2008: Week5 Day1 is done, Day2 starts

--- DMIT 2008/course material/Week 5/components-and-pages-routing/src/js/components/button/button.js
+++
@@ -1,7 +0,0 @@
-const button = function (title='ui button', className='ui-button'){
-  const element = `<button class="${className}">${title}</button>`
-
-  return element;
-}
-
-export default button
