3d5cef330646c50dd8a35349cb9046e465fd9053
Seonhye Yoon <ysunh118@gmail.com>
2021-11-29 02:55:20 +0900

    DMIT2008: Week 11 class set up

---
+++ DMIT2008/course material/Week 10 - Week 11/director/firebase/firebaseConfig.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDgsbVjwGX8ezc5hED5retU7LVmC6nUDCo",
+    authDomain: "dmit2008-d6198.firebaseapp.com",
+    databaseURL: "https://dmit2008-d6198-default-rtdb.firebaseio.com",
+    projectId: "dmit2008-d6198",
+    storageBucket: "dmit2008-d6198.appspot.com",
+    messagingSenderId: "746716745973",
+    appId: "1:746716745973:web:7d7647624b1a7e143ab5bc",
+    measurementId: "G-98NBZMNYG1"
+  };
+
+  export default firebaseConfig
